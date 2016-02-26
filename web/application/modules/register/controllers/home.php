8c472e4de44862f4b5e9d3be50f69450d0bb2acf
gratcy <palmagratcy@gmail.com>
2016-02-27 01:13:44 +0700

    captha register

--- web/application/modules/register/controllers/home.php
+++ web/application/modules/register/controllers/home.php
@@ -5,6 +5,7 @@ class Home extends MY_Controller {
 	function __construct() {
 		parent::__construct();
 		$this -> load -> model('register_model');
+		$this -> load -> helper('recaptchalib_helper');
 	}
 
 	function index() {
@@ -15,62 +16,89 @@ class Home extends MY_Controller {
 			$cemail = $this -> input -> post('cemail');
 			$pass = $this -> input -> post('pass');
 			$cpass = $this -> input -> post('cpass');
+			$captchar = $this -> rg -> post('captchar');
 			
-			if (!$name || !$phone || !$email) {
-				__set_error_msg(array('error' => 'Data that you entered is incomplete !!!'));
-				redirect(site_url('register'));
-			}
-			else if (preg_match('/^[0-9\-\+]{8-20}$/', $phone)) {
-				__set_error_msg(array('error' => 'Invalid format phone number !!!'));
-				redirect(site_url('register'));
-			}
-			else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-				__set_error_msg(array('error' => 'Invalid email format !!!'));
-				redirect(site_url('register'));
-			}
-			else if ($cemail !== $email) {
-				__set_error_msg(array('error' => 'Email and confirm email is not match !!!'));
-				redirect(site_url('register'));
-			}
-			else if ($cpass !== $pass) {
-				__set_error_msg(array('error' => 'Password and confirm password is not match !!!'));
-				redirect(site_url('register'));
-			}
-			else {
-				$salt = __get_salt();
-				$pwd = __set_pass($cpass, $salt);
-				$rcode = explode('@',$cemail);
-				
-				if ($this -> register_model -> __check_email_exists($cemail) > 0) {
-					__set_error_msg(array('error' => 'Email has been registered, please choose another email !!!'));
-					redirect(site_url('register'));
+			$params = array();
+			$params['secret'] = '6Le2UxkTAAAAAF3MwydlfwgtO-lM-YZoW5-nvowi';
+			$params['response'] = urlencode($captchar);
+			$params['remoteip'] = $_SERVER['REMOTE_ADDR'];
+
+			$params_string = http_build_query($params);
+			$requestURL = 'https://www.google.com/recaptcha/api/siteverify?' . $params_string;
+
+			$curl = curl_init();
+
+			curl_setopt_array($curl, array(
+				CURLOPT_RETURNTRANSFER => 1,
+				CURLOPT_URL => $requestURL,
+			));
+
+			$response = curl_exec($curl);
+			curl_close($curl);
+
+			$response = @json_decode($response, true);
+			if ($response) {
+				if ($response["success"] == false) {
+					echo 'Kode keamanan salah!';
 				}
 				else {
-					if ($this -> register_model -> __insert_user(array('ulevel' => 2, 'uemail' => $cemail, 'upass' => $pwd, 'usalt' => $salt))) {
-						$uid = $this -> db -> insert_id();
-						$this -> register_model -> __update_user($uid, array('urefcode' => $rcode[0] . $uid),1);
-						$this -> register_model -> __update_user($uid, array('ufullname' => $name),2);
-						
-						$key = __get_salt();
-						$this -> register_model -> __insert_token(array('tuid' => $uid, 'ttype' => 1, 'tkey' => $key, 'tstatus' => 1));
-						
-						$to = $email;
-						$subject = "Email Confirmation Activation Account Anti Block";
-						$message = "Email Confirmation Activation Account Anti Block \r\n";
-						$message = "Please click this link below to activation your account anti block \r\n";
-						$message = site_url('confirm?email=' . $cemail . '&key=' . $key) . " \r\n \r\n \r\n \r\n";
-						$message = "Regards, \r\n";
-						$message = " \r\n";
-						$message = "Admin \r\n";
-						
-						__send_email($to,$subject,$message);
-						
-						__set_error_msg(array('info' => 'Register complete, please check your email to activation account.'));
+					if (!$name || !$phone || !$email) {
+						__set_error_msg(array('error' => 'Data that you entered is incomplete !!!'));
+						redirect(site_url('register'));
+					}
+					else if (preg_match('/^[0-9\-\+]{8-20}$/', $phone)) {
+						__set_error_msg(array('error' => 'Invalid format phone number !!!'));
+						redirect(site_url('register'));
+					}
+					else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+						__set_error_msg(array('error' => 'Invalid email format !!!'));
+						redirect(site_url('register'));
+					}
+					else if ($cemail !== $email) {
+						__set_error_msg(array('error' => 'Email and confirm email is not match !!!'));
+						redirect(site_url('register'));
+					}
+					else if ($cpass !== $pass) {
+						__set_error_msg(array('error' => 'Password and confirm password is not match !!!'));
 						redirect(site_url('register'));
 					}
 					else {
-						__set_error_msg(array('error' => 'Dissmiss input data !!!'));
-						redirect(site_url('register'));
+						$salt = __get_salt();
+						$pwd = __set_pass($cpass, $salt);
+						$rcode = explode('@',$cemail);
+						
+						if ($this -> register_model -> __check_email_exists($cemail) > 0) {
+							__set_error_msg(array('error' => 'Email has been registered, please choose another email !!!'));
+							redirect(site_url('register'));
+						}
+						else {
+							if ($this -> register_model -> __insert_user(array('ulevel' => 2, 'uemail' => $cemail, 'upass' => $pwd, 'usalt' => $salt))) {
+								$uid = $this -> db -> insert_id();
+								$this -> register_model -> __update_user($uid, array('urefcode' => $rcode[0] . $uid),1);
+								$this -> register_model -> __update_user($uid, array('ufullname' => $name),2);
+								
+								$key = __get_salt();
+								$this -> register_model -> __insert_token(array('tuid' => $uid, 'ttype' => 1, 'tkey' => $key, 'tstatus' => 1));
+								
+								$to = $email;
+								$subject = "Email Confirmation Activation Account Anti Block";
+								$message = "Email Confirmation Activation Account Anti Block \r\n";
+								$message = "Please click this link below to activation your account anti block \r\n";
+								$message = site_url('confirm?email=' . $cemail . '&key=' . $key) . " \r\n \r\n \r\n \r\n";
+								$message = "Regards, \r\n";
+								$message = " \r\n";
+								$message = "Admin \r\n";
+								
+								__send_email($to,$subject,$message);
+								
+								__set_error_msg(array('info' => 'Register complete, please check your email to activation account.'));
+								redirect(site_url('register'));
+							}
+							else {
+								__set_error_msg(array('error' => 'Dissmiss input data !!!'));
+								redirect(site_url('register'));
+							}
+						}
 					}
 				}
 			}
